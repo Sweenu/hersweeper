5cd25f7d39868df971e1ab7e4f57822f1b661f25
Sweenu <brunoinec@gmail.com>
2017-11-18 19:05:32 +0100

    fixed all

--- hersweeper.py
+++ hersweeper.py
@@ -1,7 +1,7 @@
 import random
 import itertools
 
-NB_COLUMNS = 5
+NB_COLS = 5
 NB_ROWS = 3
 BOMB_PERCENTAGE = 20
 
@@ -11,62 +11,66 @@ class Game:
         self.grid = grid
 
     def init(self, bomb_percentage):
-        Grid.sample(self, 20)
-        # for i in range(self.grid.nb_rows):
-            # for j in range(self.grid.nb_columns):
-                # if random.randint(0, 100) < bomb_percentage:
-                    # self.grid[i][j].bomb = True
-        for square in self.grid.neighbor_squares(i, j):
-            square.nb += 1
+        nb_squares = self.grid.nb_cols * self.grid.nb_rows
+        nb_bombs = round((nb_squares * bomb_percentage) / 100)
+        for square in self.grid.sample(nb_bombs):
+            square.bomb = True
+
+            for square in self.grid.neighbor_squares(square):
+                square.nb += 1
 
 
 class Row:
-    def __init__(self, nb):
-        self.__row = [Square() for _ in range(nb)]
+    def __init__(self, nb, col_nb):
+        self.__row = [Square(x=i, y=col_nb) for i in range(nb)]
 
     def __repr__(self):
         return self.__row.__repr__()
 
-    def __getitem__(self, j):
-        return self.__row[j]
+    def __getitem__(self, x):
+        return self.__row[x]
 
 
 class Grid:
-    def __init__(self, nb_columns, nb_rows):
-        self.nb_columns = nb_columns
+    def __init__(self, nb_cols, nb_rows):
+        self.nb_cols = nb_cols
         self.nb_rows = nb_rows
-        self.__grid = [Row(nb_columns) for _ in range(nb_rows)]
+
+        self.__grid = [Row(nb_cols, col_nb=i) for i in range(nb_rows)]
 
     def __repr__(self):
-        return f'{self.nb_columns}x{self.nb_rows} grid'
+        return f'{self.nb_cols}x{self.nb_rows} grid'
 
-    def __getitem__(self, i):
-        return self.__grid[i]
+    def __getitem__(self, y):
+        return self.__grid[y]
 
-    def sample(self, nb_cases):
-        i = []
-        x = 0
-        for row in self.grid:
-            i.append(row)
-        while x <= nb_cases:
-            j = random.sample(i, 1)
-            k = random.sample(j, 1)
-            self.grid[j][k].bomb = True
-            x = x + 1
+    def sample(self, nb_squares):
+        squares = set()
+        while len(squares) < nb_squares:
+            y = random.randint(0, self.nb_rows - 1)
+            x = random.randint(0, self.nb_cols - 1)
+            square = self[y][x]
+            # no need to test if the square is already in the set. A set can
+            # only hold one copy of an element by nature
+            squares.add(square)
 
-    def neighbor_squares(self, i, j):
-        neighboring_squares = itertools.product(range(j-1, j+2),
-                                                range(i-1, i+2))
+        return squares
+
+    def neighbor_squares(self, square):
+        neighboring_squares = itertools.product(range(square.x-1, square.x+2),
+                                                range(square.y-1, square.y+2))
         for tup in neighboring_squares:
-            if tup != (j, i):
+            if tup != (square.x, square.y):
                 try:
-                    yield self[tup[0]][tup[1]]
+                    yield self[tup[1]][tup[0]]
                 except IndexError:
                     pass
 
 
 class Square:
-    def __init__(self, nb_neighbor_bomb=0, bomb=False, flag=False):
+    def __init__(self, x, y, nb_neighbor_bomb=0, bomb=False, flag=False):
+        self.x = x
+        self.y = y
         self.bomb = bomb
         self.flag = flag
         self.nb = nb_neighbor_bomb
@@ -81,7 +85,7 @@ class Square:
 
 
 def main():
-    grid = Grid(NB_COLUMNS, NB_ROWS)
+    grid = Grid(NB_COLS, NB_ROWS)
     game = Game(grid)
     game.init(BOMB_PERCENTAGE)
 
