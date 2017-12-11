aa419c0ee97bbf05d6845ebcc8e9a3a34dd215b6
Sweenu <brunoinec@gmail.com>
2017-12-11 17:23:24 +0100

    cli init

--- hersweeper/hersweeper.py
+++ hersweeper/hersweeper.py
@@ -1,33 +1,53 @@
 import re
 
-import core
-import display
+import click
+
+from hersweeper import core
 
 EASY = 10
 NORMAL = 20
 HARD = 30
 
 
-def main():
-    prompt = '--> '
+class CommandLine:
+    def __init__(self, prompt='--> '):
+        self.prompt = prompt
 
-    print('Size of grid (in the form <cols>x<rows>) [7x10]')
-    size = re.match(r'(\d+)x(\d+)', input(prompt) or '7x10')
+    def setup_game(self):
+        print('Size of grid (in the form <cols>x<rows>) [7x10]')
+        size = re.match(r'(\d+)x(\d+)', input(self.prompt) or '7x10')
 
-    print('Difficulty of the game (easy, normal or hard) [normal]')
-    difficulty = input(prompt) or 'normal'
-    if difficulty == 'easy':
-        bomb_percentage = EASY
-    elif difficulty == 'hard':
-        bomb_percentage = HARD
+        print('Difficulty of the game (easy, normal or hard) [normal]')
+        difficulty = input(self.prompt) or 'normal'
+        if difficulty == 'easy':
+            bomb_percentage = EASY
+        elif difficulty == 'hard':
+            bomb_percentage = HARD
+        else:
+            bomb_percentage = NORMAL
+
+        grid = core.Grid(int(size.group(1)), int(size.group(2)))
+        self.game = core.Game(grid, bomb_percentage)
+        self.game.init()
+
+    def run(self):
+        while True:
+            print("What's your next move? (h for help)")
+            input(self.prompt)
+            
+
+
+@click.command()
+@click.command('--graphic', '-g', is_flag=True,
+               help='Run the game in terminal graphic mode')
+def main(graphic):
+    if graphic:
+        print('Not implemented yet.')
     else:
-        bomb_percentage = NORMAL
+        game = CommandLine()
 
-    game = core.Game(core.Grid(int(size.group(1)), int(size.group(2))),
-                     bomb_percentage)
-    game.init()
-
-    print(game.grid)
+    game.setup_game()
+    game.run()
 
 
 if __name__ == '__main__':
